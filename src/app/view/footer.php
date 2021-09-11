a50638c1cd94a0851d7de24087200e4b1d4846cc
Jhonatan Ruiz <59362101+xjhruiz@users.noreply.github.com>
2021-09-11 22:23:34 +0200

    github project

--- src/app/view/footer.php
+++ src/app/view/footer.php
@@ -3,7 +3,7 @@
         <div class="footer-colum">
             <span class="footer-copyright">© 2021 <strong>TodoList</strong> <br>
                 <small>Un proyecto de <a href="https://www.linkedin.com/in/jhonatanruiz97/" target="_blank">Jhonatan Ruiz</a></small>
-                <small>El proyecto se encuentra en <a class="gitme" href="https://github.com/xjhruiz" target="_blank">@xjhruiz</a></small>
+                <small>El proyecto se encuentra en <a class="gitme" href="https://github.com/xjhruiz/todo-list-app" target="_blank">@xjhruiz</a></small>
             </span>
         </div>
     </div>
